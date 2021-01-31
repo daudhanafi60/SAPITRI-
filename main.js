ecc35d9b45726bb93b970369def045f847080653
DaudHanafi <76601784+daudhanafi60@users.noreply.github.com>
2021-02-01 05:03:36 +0700

    Update main.js

--- main.js
+++ main.js
@@ -11,7 +11,7 @@ let util = require('util')
 let WAConnection = simple.WAConnection(_WAConnection)
 
 
-global.owner = [6281216795577@s.whatsapp.net] // Put your number here
+global.owner = [6281216795577] // Put your number here
 global.mods = [6281216795577@s.whatsapp.net] // Want some help?
 global.prems = [6281216795577@s.whatsapp.net] // Premium user has unlimited limit
 
